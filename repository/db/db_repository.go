d150f5db5a0c61cc4bb7180d96157f62c2e821da
Kento75 <kento2github@gmail.com>
2021-06-09 07:41:00 +0900

    Add: cassandra接続設定追加

--- repository/db/db_repository.go
+++ repository/db/db_repository.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"github.com/go-sandbox/bookstore_oauth-api/clients/cassandra"
 	"github.com/go-sandbox/bookstore_oauth-api/domain/access_token"
 	"github.com/go-sandbox/bookstore_oauth-api/utils/errors"
 )
@@ -17,5 +18,12 @@ type dbRepository struct {
 }
 
 func (r *dbRepository) GetById(string) (*access_token.AccessToken, *errors.RestErr) {
+	session, err := cassandra.GetSession()
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// TODO: implement get access token from CassandraDB
 	return nil, errors.NewInternalServerError("database connection not implemented by yet")
 }
